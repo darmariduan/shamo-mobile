c0b1d45c0be6c4bc343b2731e879e5ed42c8b76f
darmariduan <dharmarhiduwhan@gmail.com>
2024-08-08 13:43:21 +0700

    18-Chat Page Bagian Content

---
+++ lib/widgets/chat_tile.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+
+class ChatTile extends StatelessWidget {
+  const ChatTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 33),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Image.asset(
+                'assets/image_shop_logo.png',
+                width: 54,
+              ),
+              SizedBox(
+                width: 12,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Shop Store',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 15,
+                      ),
+                    ),
+                    Text(
+                      'Good night, Apakah item ini masih tersedia untuk dibeli ?',
+                      overflow: TextOverflow.ellipsis,
+                      style: secondaryTextStyle.copyWith(fontWeight: light),
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                'Now',
+                style: secondaryTextStyle.copyWith(fontSize: 10),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Divider(
+            thickness: 1,
+            color: Color(0xff2B2939),
+          ),
+        ],
+      ),
+    );
+  }
+}
