c981ef5c16aff53ff3836bfbe31f5287092f7074
darmariduan <dharmarhiduwhan@gmail.com>
2024-08-09 23:05:04 +0700

    19-Detail Chat Page Bagian Header

--- lib/widgets/chat_tile.dart
+++ lib/widgets/chat_tile.dart
@@ -6,51 +6,56 @@ class ChatTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 33),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Image.asset(
-                'assets/image_shop_logo.png',
-                width: 54,
-              ),
-              SizedBox(
-                width: 12,
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Shop Store',
-                      style: primaryTextStyle.copyWith(
-                        fontSize: 15,
-                      ),
-                    ),
-                    Text(
-                      'Good night, Apakah item ini masih tersedia untuk dibeli ?',
-                      overflow: TextOverflow.ellipsis,
-                      style: secondaryTextStyle.copyWith(fontWeight: light),
-                    ),
-                  ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/detail-chat');
+      },
+      child: Container(
+        margin: EdgeInsets.only(top: 33),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Image.asset(
+                  'assets/image_shop_logo.png',
+                  width: 54,
                 ),
-              ),
-              Text(
-                'Now',
-                style: secondaryTextStyle.copyWith(fontSize: 10),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 12,
-          ),
-          Divider(
-            thickness: 1,
-            color: Color(0xff2B2939),
-          ),
-        ],
+                SizedBox(
+                  width: 12,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Shop Store',
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 15,
+                        ),
+                      ),
+                      Text(
+                        'Good night, Apakah item ini masih tersedia untuk dibeli ?',
+                        overflow: TextOverflow.ellipsis,
+                        style: secondaryTextStyle.copyWith(fontWeight: light),
+                      ),
+                    ],
+                  ),
+                ),
+                Text(
+                  'Now',
+                  style: secondaryTextStyle.copyWith(fontSize: 10),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Divider(
+              thickness: 1,
+              color: Color(0xff2B2939),
+            ),
+          ],
+        ),
       ),
     );
   }
